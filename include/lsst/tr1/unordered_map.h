b55735aed2a087dfdefbb221c85530c6231ec902
ktlim <ktlim@git.lsstcorp.org>
2009-01-13 06:35:23 +0000

    Try using boost::unordered_map instead of __gnu_cxx::hash_map or std::tr1.

--- include/lsst/tr1/unordered_map.h
+++ include/lsst/tr1/unordered_map.h
@@ -1,27 +1,14 @@
 #if !defined(LSST_TR1_UNORDERED_MAP_H)
 #define LSST_TR1_UNORDERED_MAP_H
 
-#if LSST_HAVE_TR1
-#include <tr1/unordered_map>
-#else
-#include <ext/hash_map>
-
-namespace __gnu_cxx {
-    template<> class hash<std::string> {
-    public:
-        size_t operator()(std::string const& s) const {
-            return __gnu_cxx::hash<char const*>()(s.c_str());
-        }
-    };
-}
+#include "boost/unordered_map.hpp"
 
 namespace std {
     namespace tr1 {
         template <typename KEY_t, typename VAL_t>
-        class unordered_map : public __gnu_cxx::hash_map<KEY_t, VAL_t> {
+        class unordered_map : public boost::unordered_map<KEY_t, VAL_t> {
         };
     }
 }
-#endif
 
 #endif
