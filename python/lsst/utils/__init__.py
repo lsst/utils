2e6fcb4e96535a57f4bcdb89dce15886d6bfb904
rhl <rhl@git.lsstcorp.org>
2010-06-04 18:02:13 +0000

    Added lsst.utils.version

--- python/lsst/utils/__init__.py
+++ python/lsst/utils/__init__.py
@@ -1,1 +1,26 @@
 from utilsLib import *
+
+def version(HeadURL, productName=None):
+    """Return productName's version given a HeadURL string. If a different version is setup, return that too
+
+N.b. you need to tell svn to expand that HeadURL in the first place!
+"""
+
+    version_eups = "(unknown)"
+    version_svn = guessSvnVersion(HeadURL)
+
+    if productName:
+        try:
+            import eups
+        except ImportError:
+            pass
+        else:
+            try:
+                version_eups = eups.getSetupVersion(productName)
+            except AttributeError:
+                pass
+
+    if version_eups == version_svn:
+        return version_svn
+    else:
+        return "%s (setup: %s)" % (version_svn, version_eups)
