d89d2b93cc8509e2c27c354e4ab0e7a416c4684b
Jim Bosch <jbosch@astro.princeton.edu>
2012-01-09 16:45:54 -0500

    ticket implementation finished; added Python return value utilities, stringification, and safer bool conversion, as well as unit tests

---
+++ tests/swig.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""
+Tests for SWIG utilities
+
+Run with:
+   python swig.py
+or
+   python
+   >>> import swig; swig.run()
+"""
+
+import eups
+import os
+import pdb  # we may want to say pdb.set_trace()
+import sys
+import unittest
+
+import lsst.utils.tests as utilsTests
+import testLib
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class SwigTestCase(unittest.TestCase):
+    """A test case for SWIG utilities in p_lsstSwig.i"""
+
+    def setUp(self):
+        self.example = testLib.Example("foo")
+
+    def testConstructors(self):
+        self.assertEqual(self.example.getValue(), "foo")
+        self.assertRaises(NotImplementedError, testLib.Example, [5])
+        self.assertEqual(testLib.Example("bar").getValue(), "bar")
+    
+    def testReturnNone(self):
+        result = self.example.get1()
+        self.assert_(result is None)
+
+    def testReturnSelf(self):
+        result = self.example.get2()
+        self.assert_(result is self.example)
+    
+    def testReturnCopy(self):
+        result = self.example.get3()
+        self.assert_(result is not self.example)
+        self.assert_(type(result) == testLib.Example)
+        result.setValue("bar")
+        self.assertEqual(self.example.getValue(), "foo")
+
+    def testStringification(self):
+        s = "Example(foo)"
+        self.assertEqual(str(self.example), s)
+        self.assertEqual(repr(self.example), s)
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+
+    utilsTests.init()
+
+    suites = []
+    suites += unittest.makeSuite(SwigTestCase)
+    suites += unittest.makeSuite(utilsTests.MemoryTestCase)
+    return unittest.TestSuite(suites)
+
+def run(exit=False):
+    """Run the tests"""
+    utilsTests.run(suite(), exit)
+
+if __name__ == "__main__":
+    run(True)
---
+++ tests/testLib.i
@@ -0,0 +1,71 @@
+// -*- lsst-c++ -*-
+
+/* 
+ * LSST Data Management System
+ * Copyright 2008, 2009, 2010 LSST Corporation.
+ * 
+ * This product includes software developed by the
+ * LSST Project (http://www.lsst.org/).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the LSST License Statement and 
+ * the GNU General Public License along with this program.  If not, 
+ * see <http://www.lsstcorp.org/LegalNotices/>.
+ */
+ 
+%define testLib_DOCSTRING
+"
+Test module for various utilities in p_lsstSwig.i
+"
+%enddef
+
+%feature("autodoc", "1");
+%module(package="testLib", docstring=testLib_DOCSTRING) testLib
+
+%pythonnondynamic;
+%naturalvar;  // use const reference typemaps
+
+%include "lsst/p_lsstSwig.i"
+
+%lsst_exceptions()
+
+%returnNone(Example::get1)
+%returnSelf(Example::get2)
+%returnCopy(Example::get3)
+%addStreamRepr(Example)
+
+%inline %{
+    class Example {
+    public:
+
+        explicit Example(std::string const & v) : _value(v) {}
+
+        explicit Example(bool b) : _value("boolean") {}
+
+        Example(Example const & other) : _value(other._value) {}
+
+        Example & get1() { return *this; }
+        Example & get2() { return *this; }
+        Example & get3() { return *this; }
+
+        std::string getValue() const { return _value; }
+        void setValue(std::string const & v) { _value = v; }
+        
+    private:
+        std::string _value;
+    };
+
+    inline std::ostream & operator<<(std::ostream & os, Example const & ex) {
+        return os << "Example(" << ex.getValue() << ")";
+    }
+
+%}
