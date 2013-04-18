43dcda05fd596eb9951876fb80e4a4a199a14bdc
Jim Bosch <jbosch@astro.princeton.edu>
2013-04-18 15:46:47 -0400

    Add configurable floating point comparisons to test utilities.

--- python/lsst/utils/tests.py
+++ python/lsst/utils/tests.py
@@ -23,11 +23,12 @@
 """Support code for running unit tests"""
 
 import unittest
+import numpy
 try:
     import lsst.daf.base as dafBase
 except ImportError:
     dafBase = None
-    
+
 import lsst.pex.exceptions as pexExcept
 import os
 import sys
@@ -179,3 +180,111 @@ def debugger(*exceptions):
                 pdb.post_mortem(sys.exc_info()[2])
         return wrapper
     return decorator
+
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+def assertClose(testCase, lhs, rhs, rtol=sys.float_info.epsilon, atol=sys.float_info.epsilon, relTo=None,
+                printFailures=True, plotOnFailure=False, plotFileName=None, invert=False):
+    """Highly-configurable floating point comparisons for scalars and arrays.
+
+    The test assertion will fail if all elements lhs and rhs are not equal to within the tolerances
+    specified by rtol and atol.  More precisely, the comparison is:
+
+    abs(lhs - rhs) <= relTo*rtol OR abs(lhs - rhs) <= atol
+
+    If rtol or atol is None, that term in the comparison is not performed at all.
+
+    When not specified, relTo is the elementwise maximum of the absolute values of lhs and rhs.
+
+    @param[in]  testCase       unittest.TestCase instance the test is part of
+    @param[in]  lhs            LHS value(s) to compare; may be a scalar or a numpy array of any dimension
+    @param[in]  rhs            RHS value(s) to compare; may be a scalar or a numpy array of any dimension
+    @param[in]  rtol           Relative tolerance for comparison; defaults to double-precision epsilon.
+    @param[in]  atol           Absolute tolerance for comparison; defaults to double-precision epsilon.
+    @param[in]  relTo          Value to which comparison with rtol is relative.
+    @param[in]  printFailures  Upon failure, print all inequal elements as part of the message.
+    @param[in]  plotOnFailure  Upon failure, plot the originals and their residual with matplotlib.
+                               Only 2-d arrays are supported.
+    @param[in]  plotFileName   Filename to save the plot to.  If None, the plot will be displayed in a
+                               a window.
+    @param[in]  invert         If True, invert the comparison and fail only if any elements *are* equal.
+                               Used to implement assertNotClose, which should generally be used instead
+                               for clarity.
+    """
+    diff = lhs - rhs
+    absDiff = numpy.abs(lhs - rhs)
+    if rtol is not None:
+        if relTo is None:
+            relTo = numpy.maximum(numpy.abs(lhs), numpy.abs(rhs))
+        else:
+            relTo = numpy.abs(relTo)
+        bad = absDiff > rtol*relTo
+        if atol is not None:
+            bad = numpy.logical_and(bad, absDiff > atol)
+    else:
+        if atol is None:
+            raise ValueError("rtol and atol cannot both be None")
+        bad = absDiff > atol
+    failed = numpy.any(bad)
+    if invert:
+        failed = not failed
+        bad = numpy.logical_not(bad)
+        cmpStr = "=="
+        failStr = "are the same"
+    else:
+        cmpStr = "!="
+        failStr = "differ"
+    msg = []
+    if failed:
+        if numpy.isscalar(bad):
+            msg = ["%r %s %r with rtol=%s, atol=%s" % (lhs, cmpStr, rhs, rtol, atol)]
+        else:
+            msg = ["%d/%d elements %s with rtol=%s, atol=%s"
+                   % (bad.sum(), bad.size, failStr, rtol, atol)]
+            if plotOnFailure:
+                if len(lhs.shape) != 2 or len(rhs.shape) != 2:
+                    raise ValueError("plotOnFailure is only valid for 2-d arrays")
+                kwargs = dict(
+                    )
+                from matplotlib import pyplot
+                pyplot.figure()
+                xg, yg = numpy.meshgrid(numpy.arange(bad.shape[1]), numpy.arange(bad.shape[0]))
+                badX = xg[bad]
+                badY = yg[bad]
+                vmin1 = numpy.minimum(numpy.min(lhs), numpy.min(rhs))
+                vmax1 = numpy.maximum(numpy.max(lhs), numpy.max(rhs))
+                vmin2 = numpy.min(diff)
+                vmax2 = numpy.max(diff)
+                for n, (image, title) in enumerate([(lhs, "lhs"), (rhs, "rhs"), (diff, "diff")]):
+                    pyplot.subplot(2,3,n+1)
+                    im1 = pyplot.imshow(image, cmap=pyplot.cm.gray, interpolation='nearest', origin='lower',
+                                        vmin=vmin1, vmax=vmax1)
+                    pyplot.plot(badX, badY, 'rx')
+                    pyplot.axis("off")
+                    pyplot.title(title)
+                    pyplot.subplot(2,3,n+4)
+                    im2 = pyplot.imshow(image, cmap=pyplot.cm.gray, interpolation='nearest', origin='lower',
+                                        vmin=vmin2, vmax=vmax2)
+                    pyplot.plot(badX, badY, 'rx')
+                    pyplot.axis("off")
+                    pyplot.title(title)
+                pyplot.subplots_adjust(left=0.05, bottom=0.05, top=0.92, right=0.75, wspace=0.05, hspace=0.05)
+                cax1 = pyplot.axes([0.8, 0.55, 0.05, 0.4])
+                pyplot.colorbar(im1, cax=cax1)
+                cax2 = pyplot.axes([0.8, 0.05, 0.05, 0.4])
+                pyplot.colorbar(im2, cax=cax2)
+                if plotFileName:
+                    pyplot.savefig(plotFileName)
+                else:
+                    pyplot.show()
+            if printFailures:
+                for a, b in zip(lhs[bad], rhs[bad]):
+                    msg.append("%r %s %r" % (a, cmpStr, b))
+    testCase.assertFalse(failed, msg="\n".join(msg))
+
+def assertNotClose(testCase, lhs, rhs, **kwds):
+    """Fail a test if the given floating point values are completely equal to within the given tolerances.
+
+    See assertClose for more information.
+    """
+    return assertClose(testCase, lhs, rhs, invert=True, **kwds)
--- tests/utils.py
+++ tests/utils.py
@@ -37,6 +37,7 @@ import os
 import pdb  # we may want to say pdb.set_trace()
 import sys
 import unittest
+import numpy
 
 import lsst.utils.tests as utilsTests
 import lsst.pex.exceptions as pexExcept
@@ -73,6 +74,32 @@ class UtilsTestCase(unittest.TestCase):
             utilsLib.productDir("XXX utils XXX non existent")
 
         utilsTests.assertRaisesLsstCpp(self, pexExcept.NotFoundException, tst)
+
+    def testCompareArrays(self):
+        utilsTests.assertClose(self, 0.0, 0.0)
+        utilsTests.assertClose(self, 0.0, 1E-8, atol=1E-7)
+        utilsTests.assertNotClose(self, 0.0, 1E-8, atol=1E-9)
+        utilsTests.assertClose(self, 100, 100 + 1E-8, rtol=1E-7)
+        utilsTests.assertNotClose(self, 100, 100 + 1E-8, rtol=1E-12)
+        a = numpy.zeros((5, 5), dtype=float)
+        b = numpy.zeros((5, 5), dtype=float)
+        utilsTests.assertClose(self, a, b)
+        b[:,:] = 1E-8
+        utilsTests.assertClose(self, a, b, atol=1E-7)
+        utilsTests.assertNotClose(self, a, b, atol=1E-9)
+        a[:,:] = 100.0
+        b[:,:] = 100.0 + 1E-8
+        utilsTests.assertClose(self, a, b, rtol=1E-7)
+        utilsTests.assertNotClose(self, a, b, rtol=1E-12)
+        a[:,:] = numpy.arange(-12,13).reshape(5,5)
+        b[:,:] = a
+        b[2,:] += numpy.linspace(-1E-4, 1E-4, 5)
+        utilsTests.assertClose(self, a, b, rtol=1E-3, atol=1E-4)
+        if False:
+            # set to True to test plotting and printing by-eye when tests fail
+            # should see failures on the center row of the 5x5 image, but not the very center point
+            utilsTests.assertClose(self, a, b, rtol=1E-6, plotOnFailure=True)
+        
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
