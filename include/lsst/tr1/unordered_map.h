5672396b1d6068bb54c94380338af7923faaa813
Ian Sullivan <sullivan@D-173-250-202-27.dhcp4.washington.edu>
2015-10-06 15:23:28 -0700

    Change boost::unordered_map to std

--- include/lsst/tr1/unordered_map.h
+++ include/lsst/tr1/unordered_map.h
@@ -22,12 +22,11 @@
  
 #if !defined(LSST_TR1_UNORDERED_MAP_H)
 #define LSST_TR1_UNORDERED_MAP_H
-
-#include "boost/unordered_map.hpp"
+#include <unordered_map>
 
 namespace std {
     namespace tr1 {
-        using ::boost::unordered_map;
+        using ::std::unordered_map;
     }
 }
 
