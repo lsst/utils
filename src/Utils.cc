4be585782bb66b8135bffc27eb6d8d2851360e82
Russell Owen <rowen@uw.edu>
2015-11-18 16:39:58 -0800

    Remove unused function "stringToAny"

--- src/Utils.cc
+++ src/Utils.cc
@@ -31,38 +31,6 @@
 namespace lsst {
 namespace utils {
 
-boost::any stringToAny(std::string valueString)
-{
-    const boost::regex intRegex("(\\Q+\\E|\\Q-\\E){0,1}[0-9]+");
-    const boost::regex doubleRegex("(\\Q+\\E|\\Q-\\E){0,1}([0-9]*\\.[0-9]+|[0-9]+\\.[0-9]*)((e|E)(\\Q+\\E|\\Q-\\E){0,1}[0-9]+){0,1}");
-    const boost::regex FitsStringRegex("'(.*)'");
-
-    boost::smatch matchStrings;
-
-    std::istringstream converter(valueString);
-
-    if (boost::regex_match(valueString, intRegex)) {
-        // convert the string to an int
-        int intVal;
-        converter >> intVal;
-        return boost::any(intVal);
-    }
-        
-    if (boost::regex_match(valueString, doubleRegex)) {
-        // convert the string to a double
-        double doubleVal;
-        converter >> doubleVal;
-        return boost::any(doubleVal);
-    }
-
-    if (boost::regex_match(valueString, matchStrings, FitsStringRegex)) {
-        // strip off the enclosing single quotes and return the string
-        return boost::any(matchStrings[1].str());
-    }
-
-    return boost::any(valueString);
-}
-
 std::string getPackageDir(std::string const& packageName) {
     std::string envVar = packageName;      // package's environment variable
     
