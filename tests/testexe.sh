1da760ca17ffebd5ce874e0d5a1d1b5c9d9089f7
Tim Jenness <tjenness@lsst.org>
2018-06-05 11:46:53 -0700

    Improve coverage of executables test

--- tests/testexe.sh
+++ tests/testexe.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-# Simply executable shell script designed to do
-# nothing except exit with 0 status.
+# Simple executable shell script.
+# By default does nothing and exits with 0 status.
+# with "-f" option, exits with a status of 1
 
+while getopts "fh" opt; do
+  case "$opt" in
+  f)
+    echo "Triggering bad exit status"
+    exit 1
+    ;;
+  *)
+    ;;
+  esac
+done
+
+echo "Completing test script without error"
