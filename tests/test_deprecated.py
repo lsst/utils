ebf1713e11684b6c703bab90214a82a31fc1f5fa
Kian-Tat Lim <ktl@slac.stanford.edu>
2019-04-23 01:14:14 -0500

    Fix indents.

--- tests/test_deprecated.py
+++ tests/test_deprecated.py
@@ -24,20 +24,23 @@ import lsst.utils.tests
 
 import lsst.utils
 
+
 class DeprecatedTestCase(lsst.utils.tests.TestCase):
     def test_deprecate_pybind11(self):
         def old(x):
             """Docstring"""
             return x + 1
         # Use an unusual category
-        old = lsst.utils.deprecate_pybind11(old,
-                reason="For testing.", category=PendingDeprecationWarning)
-        with self.assertWarnsRegex(PendingDeprecationWarning,
+        old = lsst.utils.deprecate_pybind11(
+            old, reason="For testing.", category=PendingDeprecationWarning)
+        with self.assertWarnsRegex(
+                PendingDeprecationWarning,
                 r"Call to deprecated function \(or staticmethod\) old\. \(For testing\.\)$"):
             # Check that the function still works
             self.assertEqual(old(3), 4)
         self.assertIn("Docstring", old.__doc__)
         self.assertIn("For testing.", old.__doc__)
+
 
 if __name__ == "__main__":
     unittest.main()
