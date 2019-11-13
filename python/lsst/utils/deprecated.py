7bafdab7a7c553be6c6dfa0e3810a18344d4ebc1
Leanne Guy <leanne.guy@lsst.org>
2019-11-13 10:32:06 -0600

    Update doc string to specify this works on classes as well as functions and methods

--- python/lsst/utils/deprecated.py
+++ python/lsst/utils/deprecated.py
@@ -24,7 +24,7 @@ import functools
 
 
 def deprecate_pybind11(func, reason, category=FutureWarning):
-    """Deprecate a pybind11-wrapped C++ interface function or method.
+    """Deprecate a pybind11-wrapped C++ interface function, method or class.
 
     This needs to use a pass-through Python wrapper so that
     `~deprecated.sphinx.deprecated` can update its docstring; pybind11
