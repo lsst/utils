f62ad1c998ec2fb5f019749f87cf1bdeb9f7e550
Leanne Guy <leanne.guy@lsst.org>
2019-12-07 14:56:05 -0600

    Updated example of deprecating a class

--- python/lsst/utils/deprecated.py
+++ python/lsst/utils/deprecated.py
@@ -1,4 +1,4 @@
-# This file is part of lsst.utils.
+s file is part of lsst.utils.
 #
 # Developed for the LSST Data Management System.
 # This product includes software developed by the LSST Project
@@ -23,7 +23,7 @@ import deprecated.sphinx
 import functools
 
 
-def deprecate_pybind11(obj, reason, category=FutureWarning):
+def deprecate_pybind11(func, reason, category=FutureWarning):
     """Deprecate a pybind11-wrapped C++ interface function, method or class.
 
     This needs to use a pass-through Python wrapper so that
@@ -35,7 +35,7 @@ def deprecate_pybind11(obj, reason, category=FutureWarning):
 
     Parameters
     ----------
-    obj : function, method, or class
+    func : function, method, or class
         The function, method, or class to deprecate.
     reason : `str`
         Reason for deprecation, passed to `~deprecated.sphinx.deprecated`
@@ -44,16 +44,18 @@ def deprecate_pybind11(obj, reason, category=FutureWarning):
 
     Returns
     -------
-    obj : function, method or class
-        Wrapped function, method or class
+    func : function
+        Wrapped function
 
-    Example
+    Examples
     -------
+    .. code-block:: python
+
     ExposureF.getCalib = deprecate_pybind11(ExposureF.getCalib,
             reason="Replaced by getPhotoCalib. (Will be removed in 18.0)",
             category=FutureWarning))
     """
-    @functools.wraps(obj)
+    @functools.wraps(func)
     def internal(*args, **kwargs):
         return func(*args, **kwargs)
     return deprecated.sphinx.deprecated(reason=reason, category=category)(internal)
