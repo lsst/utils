567cf51991ad367a135e75d966aeb9843e9e33e6
Jonathan Sick <jsick@lsst.org>
2020-01-23 12:48:58 -0500

    Add __all__ to lsst.utils.deprecated

--- python/lsst/utils/deprecated.py
+++ python/lsst/utils/deprecated.py
@@ -19,6 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+__all__ = ['deprecate_pybind11', 'suppress_deprecations']
+
 import deprecated.sphinx
 import functools
 import unittest.mock
