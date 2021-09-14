b20df2c85c0532ae96e22707a43bdc83def5d2d0
Tim Jenness <tjenness@lsst.org>
2021-09-14 15:38:53 -0700

    Update license and copyright
    Now uses BSD 3 clause

--- python/lsst/utils/backtrace/__init__.py
+++ python/lsst/utils/backtrace/__init__.py
@@ -1,3 +1,14 @@
+# This file is part of utils.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 try:
     # For now, ensure that backtrace has been imported if somebody
     # is relying on it from a lsst.utils import. Treat it as an optional
--- python/lsst/utils/tests.py
+++ python/lsst/utils/tests.py
@@ -1,25 +1,14 @@
+# This file is part of utils.
 #
-# LSST Data Management System
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
 #
-# Copyright 2008-2017  AURA/LSST.
-#
-# This product includes software developed by the
-# LSST Project (http://www.lsst.org/).
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the LSST License Statement and
-# the GNU General Public License along with this program.  If not,
-# see <https://www.lsstcorp.org/LegalNotices/>.
-#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 """Support code for running unit tests"""
 
 __all__ = ["init", "MemoryTestCase", "ExecutablesTestCase", "getTempFilePath",
