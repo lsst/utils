468a34e4fd0f19528ef0b5268a43d5f8ff359649
Tim Jenness <tjenness@lsst.org>
2021-09-27 20:17:01 -0700

    Make timeMethod and others more generic
    Takes non-PropertyList and allows the decorator to take parameters.
    Include some tests.

--- python/lsst/utils/timer.py
+++ python/lsst/utils/timer.py
@@ -20,45 +20,83 @@ import resource
 import time
 import datetime
 
+from typing import (
+    Any,
+    MutableMapping,
+)
+
+
+def _add_to_metadata(metadata: MutableMapping, name: str, value: Any) -> None:
+    """Add a value to dict-like object, creating list as needed.
+
+    The list grows as more values are added for that key.
+
+    Parameters
+    ----------
+    metadata : `dict`-like, optional
+        `dict`-like object that can store keys. Uses `add()` method if
+        one is available, else creates list and appends value if needed.
+    name : `str`
+        The key to use in the metadata dictionary.
+    value : Any
+        Value to store in the list.
+    """
+    try:
+        try:
+            # PropertySet should always prefer LongLong for integers
+            metadata.addLongLong(name, value)
+        except TypeError:
+            metadata.add(name, value)
+    except AttributeError:
+        pass
+    else:
+        return
+
+    # Fallback code where `add` is not implemented.
+    if name not in metadata:
+        metadata[name] = []
+    metadata[name].append(value)
+
 
 def logPairs(obj, pairs, logLevel=logging.DEBUG, metadata=None, logger=None):
     """Log ``(name, value)`` pairs to ``obj.metadata`` and ``obj.log``
 
     Parameters
     ----------
-    obj : `lsst.pipe.base.Task`-type
-        A `~lsst.pipe.base.Task` or any other object with these two attributes:
+    obj : `object`
+        An object with one or both of these two attributes:
 
-        - ``metadata`` an instance of `lsst.daf.base.PropertyList`` (or other
-          object with ``add(name, value)`` method).
+        - ``metadata`` a `dict`-like container for storing metadata.
+          Can use the ``add(name, value)`` method if found, else will append
+          entries to a list.
         - ``log`` an instance of `logging.Logger` or subclass.
 
         If `None`, at least one of ``metadata`` or ``logger`` should be passed
         or this function will do nothing.
-
     pairs : sequence
         A sequence of ``(name, value)`` pairs, with value typically numeric.
     logLevel : `int, optional
         Log level (an `logging` level constant, such as `logging.DEBUG`).
-    metadata : `lsst.daf.base.PropertyList`, optional
+    metadata : `collections.abc.MutableMapping`, optional
         Metadata object to write entries to.  Ignored if `None`.
     logger : `logging.Logger`
         Log object to write entries to.  Ignored if `None`.
     """
     if obj is not None:
         if metadata is None:
-            metadata = obj.metadata
+            try:
+                metadata = obj.metadata
+            except AttributeError:
+                pass
         if logger is None:
-            logger = obj.log
+            try:
+                logger = obj.log
+            except AttributeError:
+                pass
     strList = []
     for name, value in pairs:
         if metadata is not None:
-            try:
-                # Use LongLong explicitly here in case an early value in the
-                # sequence is int-sized
-                metadata.addLongLong(name, value)
-            except TypeError:
-                metadata.add(name, value)
+            _add_to_metadata(metadata, name, value)
         strList.append(f"{name}={value}")
     if logger is not None:
         logging.getLogger("timer." + logger.name).log(logLevel, "; ".join(strList))
@@ -69,23 +107,23 @@ def logInfo(obj, prefix, logLevel=logging.DEBUG, metadata=None, logger=None):
 
     Parameters
     ----------
-    obj : `lsst.pipe.base.Task`-type or `None`
-        A `~lsst.pipe.base.Task` or any other object with these two attributes:
+    obj : `object`
+        An object with both or one these two attributes:
 
-        - ``metadata`` an instance of `lsst.daf.base.PropertyList`` (or other
-          object with ``add(name, value)`` method).
+        - ``metadata`` a `dict`-like container for storing metadata.
+          Can use the ``add(name, value)`` method if found, else will append
+          entries to a list.
         - ``log`` an instance of `logging.Logger` or subclass.
 
         If `None`, at least one of ``metadata`` or ``logger`` should be passed
         or this function will do nothing.
-
     prefix
         Name prefix, the resulting entries are ``CpuTime``, etc.. For example
-        timeMethod uses ``prefix = Start`` when the method begins and
+        `timeMethod` uses ``prefix = Start`` when the method begins and
         ``prefix = End`` when the method ends.
     logLevel : optional
-        Log level (an `logging` level constant, such as `logging.DEBUG`).
-    metadata : `lsst.daf.base.PropertyList`, optional
+        Log level (a `logging` level constant, such as `logging.DEBUG`).
+    metadata : `collections.abc.MutableMapping`, optional
         Metadata object to write entries to, overriding ``obj.metadata``.
     logger : `logging.Logger`
         Log object to write entries to, overriding ``obj.log``.
@@ -104,12 +142,16 @@ def logInfo(obj, prefix, logLevel=logging.DEBUG, metadata=None, logger=None):
     processes are excluded.
     """
     cpuTime = time.process_time()
-    utcStr = datetime.datetime.utcnow().isoformat()
     res = resource.getrusage(resource.RUSAGE_SELF)
     if metadata is None and obj is not None:
-        metadata = obj.metadata
+        try:
+            metadata = obj.metadata
+        except AttributeError:
+            pass
     if metadata is not None:
-        metadata.add(name=prefix + "Utc", value=utcStr)  # log messages already have timestamps
+        # Log messages already have timestamps.
+        utcStr = datetime.datetime.utcnow().isoformat()
+        _add_to_metadata(metadata, name=prefix + "Utc", value=utcStr)
     logPairs(obj=obj,
              pairs=[
                  (prefix + "CpuTime", cpuTime),
@@ -128,26 +170,36 @@ def logInfo(obj, prefix, logLevel=logging.DEBUG, metadata=None, logger=None):
              logger=logger)
 
 
-def timeMethod(func):
-    """Decorator to measure duration of a task method.
+def timeMethod(_func=None, *, metadata=None, logger=None,
+               logLevel=logging.DEBUG):
+    """Decorator to measure duration of a method.
 
     Parameters
     ----------
     func
         The method to wrap.
+    metadata : `collections.abc.MutableMapping`, optional
+        Metadata to use as override if the instance object attached
+        to this timer does not support a ``metadata`` property.
+    logger : `logging.Logger`, optional
+        Logger to use when the class containing the decorated method does not
+        have a ``log`` property.
+    logLevel : `int`, optional
+        Log level to use when logging messages. Default is `~logging.DEBUG`.
 
     Notes
     -----
-    Writes various measures of time and possibly memory usage to the task's
+    Writes various measures of time and possibly memory usage to the
     metadata; all items are prefixed with the function name.
 
     .. warning::
 
-       This decorator only works with instance methods of Task, or any class
+       This decorator only works with instance methods of any class
        with these attributes:
 
-       - ``metadata``: an instance of `lsst.daf.base.PropertyList` (or other
-         object with ``add(name, value)`` method).
+       - ``metadata``: an instance of `collections.abc.Mapping`. The ``add``
+         method will be used if available, else entries will be added to a
+         list.
        - ``log``: an instance of `logging.Logger` or subclass.
 
     Examples
@@ -156,21 +208,30 @@ def timeMethod(func):
 
     .. code-block:: python
 
+        import lsst.utils as utils
         import lsst.pipe.base as pipeBase
         class FooTask(pipeBase.Task):
             pass
 
-            @pipeBase.timeMethod
+            @utils.timeMethod
             def run(self, ...): # or any other instance method you want to time
                 pass
     """
 
-    @functools.wraps(func)
-    def wrapper(self, *args, **keyArgs):
-        logInfo(obj=self, prefix=func.__name__ + "Start")
-        try:
-            res = func(self, *args, **keyArgs)
-        finally:
-            logInfo(obj=self, prefix=func.__name__ + "End")
-        return res
-    return wrapper
+    def decorator_timer(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **keyArgs):
+            logInfo(obj=self, prefix=func.__name__ + "Start", metadata=metadata, logger=logger,
+                    logLevel=logLevel)
+            try:
+                res = func(self, *args, **keyArgs)
+            finally:
+                logInfo(obj=self, prefix=func.__name__ + "End", metadata=metadata, logger=logger,
+                        logLevel=logLevel)
+            return res
+        return wrapper
+
+    if _func is None:
+        return decorator_timer
+    else:
+        return decorator_timer(_func)
---
+++ tests/test_timer.py
@@ -0,0 +1,139 @@
+# This file is part of utils.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
+import unittest
+import logging
+import time
+import datetime
+from dataclasses import dataclass
+
+from lsst.utils.timer import timeMethod, logPairs
+
+log = logging.getLogger("test_timer")
+
+# Only use this in a single test but needs to be associated
+# with the function up front.
+test_metadata = {}
+
+
+@dataclass
+class Example1:
+    log: logging.Logger
+    metadata: dict
+
+    @timeMethod
+    def sleeper(self, duration: float) -> None:
+        """Sleep for some time."""
+        time.sleep(duration)
+
+
+@timeMethod
+def decorated_sleeper_nothing(self, duration: float) -> None:
+    time.sleep(duration)
+
+
+@timeMethod(logger=log)
+def decorated_sleeper_logger(self, duration: float) -> None:
+    time.sleep(duration)
+
+
+@timeMethod(logger=log, logLevel=logging.INFO)
+def decorated_sleeper_logger_level(self, duration: float) -> None:
+    time.sleep(duration)
+
+
+@timeMethod(metadata=test_metadata)
+def decorated_sleeper_metadata(self, duration: float) -> None:
+    time.sleep(duration)
+
+
+class TestTimeMethod(unittest.TestCase):
+
+    def testLogPairs(self):
+        # Test the non-obj case.
+        logger = logging.getLogger("test")
+        pairs = (("name1", 0), ("name2", 1))
+        metadata = {}
+        with self.assertLogs(level=logging.INFO) as cm:
+            logPairs(None, pairs, logLevel=logging.INFO, logger=logger, metadata=metadata)
+        self.assertEqual(len(cm.output), 1, cm.output)
+        self.assertTrue(cm.output[0].endswith("name1=0; name2=1"), cm.output)
+        self.assertEqual(metadata, {"name1": [0], "name2": [1]})
+
+    def assertTimer(self, duration, task):
+        # Call it twice to test the "add" functionality.
+        task.sleeper(duration)
+        task.sleeper(duration)
+        counter = 2
+
+        has_logger = getattr(task, "log", None) is not None \
+            and task.log is not None
+        has_metadata = getattr(task, "metadata", None) is not None \
+            and task.metadata is not None
+
+        if has_logger:
+            counter += 1
+            with self.assertLogs("timer.task", level=logging.DEBUG):
+                task.sleeper(duration)
+
+        if has_metadata:
+            self.assertEqual(len(task.metadata["sleeperStartUserTime"]), counter)
+
+            start = datetime.datetime.fromisoformat(task.metadata["sleeperStartUtc"][1])
+            end = datetime.datetime.fromisoformat(task.metadata["sleeperEndUtc"][1])
+            delta = end - start
+            delta_sec = delta.seconds + (delta.microseconds / 1e6)
+            self.assertGreaterEqual(delta_sec, duration)
+
+    def testTaskLike(self):
+        """Test timer on something that looks like a Task."""
+
+        # Call with different parameters.
+        parameters = ((logging.getLogger("task"), {}),
+                      (logging.getLogger("task"), None),
+                      (None, {}),
+                      (None, None))
+
+        duration = 0.1
+        for log, metadata in parameters:
+            with self.subTest(log=log, metadata=metadata):
+                task = Example1(log=log, metadata=metadata)
+                self.assertTimer(duration, task)
+
+    def testDecorated(self):
+        """Test timeMethod on non-Task like instances."""
+        duration = 0.1
+
+        # The "self" object shouldn't be usable but this should do nothing
+        # and not crash.
+        decorated_sleeper_nothing(self, duration)
+
+        # Use a function decorated for logging.
+        with self.assertLogs("timer.test_timer", level=logging.DEBUG):
+            decorated_sleeper_logger(self, duration)
+
+        # And adjust the log level
+        with self.assertLogs("timer.test_timer", level=logging.INFO):
+            decorated_sleeper_logger_level(self, duration)
+
+        # Use a function decorated for metadata.
+        self.assertEqual(len(test_metadata), 0)
+        with self.assertLogs("timer.test_timer", level=logging.DEBUG) as cm:
+            # Check that we only get a single log message and nothing from
+            # timeMethod itself.
+            decorated_sleeper_metadata(self, duration)
+            logging.getLogger("timer.test_timer").debug("sentinel")
+        self.assertEqual(len(cm.output), 1)
+        self.assertIn("decorated_sleeper_metadataStartUserTime", test_metadata)
+
+
+if __name__ == "__main__":
+    unittest.main()
