c5f3fba85f3e9b08df7baebef3de1065c55b2afc
Tim Jenness <tjenness@lsst.org>
2022-03-10 08:46:21 -0700

    Test that we really did set to 1 thread

--- tests/test_threads.py
+++ tests/test_threads.py
@@ -15,6 +15,15 @@ import unittest
 
 from lsst.utils.threads import disable_implicit_threading, set_thread_envvars
 
+try:
+    import numexpr
+except ImportError:
+    numexpr = None
+try:
+    import threadpoolctl
+except ImportError:
+    threadpoolctl = None
+
 
 class ThreadsTestCase(unittest.TestCase):
     """Tests for threads."""
@@ -27,6 +36,14 @@ class ThreadsTestCase(unittest.TestCase):
 
         disable_implicit_threading()
         self.assertEqual(os.environ["OMP_NUM_THREADS"], "1")
+
+        # Check that we have only one thread.
+        if numexpr:
+            self.assertEqual(numexpr.utils.get_num_threads(), 1)
+        if threadpoolctl:
+            info = threadpoolctl.threadpool_info()
+            for api in info:
+                self.assertEqual(api["num_threads"], 1, f"API: {api}")
 
 
 if __name__ == "__main__":
