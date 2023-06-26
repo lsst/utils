35d08c9e922a1cf5fbee185ceb5d466799a56773
Tim Jenness <tjenness@lsst.org>
2023-06-26 09:17:09 -0700

    Remove unnecessary list comprehension

--- python/lsst/utils/tests.py
+++ python/lsst/utils/tests.py
@@ -154,7 +154,7 @@ class MemoryTestCase(unittest.TestCase):
             and not f.endswith("astropy.log")
             and not f.endswith("mime/mime.cache")
             and not f.endswith(".sqlite3")
-            and not any([re.search(r, f) for r in self.ignore_regexps])
+            and not any(re.search(r, f) for r in self.ignore_regexps)
         }
 
         diff = now_open.difference(open_files)
