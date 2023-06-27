258de0bc3e72e2790fa318b0b6ab9d7715da4c0f
Tim Jenness <tjenness@lsst.org>
2023-06-27 10:52:14 -0700

    Add sphinx nitpick configuration

--- doc/conf.py
+++ doc/conf.py
@@ -11,3 +11,17 @@ html_title = project
 html_short_title = project
 doxylink = {}
 exclude_patterns = ["changes/*"]
+nitpick_ignore_regex = [
+    ("py:.*", r"lsst\..*"),  # Ignore warnings from links to other lsst packages.
+    ("py:.*", "cProfile.Profile"),  # Link does not resolve to py3 docs.
+    ("py:class", "a set-like.*"),  # collections.abc.Mapping inheritance.
+    ("py:class", "a shallow copy.*"),  # collections.abc.Mapping inheritance.
+    ("py:class", r"None\.  .*"),  # collections.abc.Mapping inheritance.
+    ("py:class", "an object providing.*"),  # collections.abc.Mapping inheritance.
+]
+nitpick_ignore = [
+    ("py:class", "module"),  # Sphinx has problems with types.ModuleType.
+    ("py:obj", "logging.DEBUG"),  # Python 3 constant can not be found.
+    ("py:class", "unittest.case.TestCase"),  # Sphinx can not see TestCase.
+    ("py:obj", "deprecated.sphinx.deprecated"),  # No intersphinx for deprecated package.
+]
